4055c8b37a18b783257fbc65d89761d6b9a3e494
evelynespinozamacias <evelynespinozamacias@gmail.com>
2021-05-13 21:31:49 -0500

    Commit Assignments

---
+++ src/Assignment1_1.java
@@ -0,0 +1,13 @@
+public class Assignment1_1 {
+
+    public static void main(String[] args) {
+        //print out my name
+        System.out.println("Evelyn Espinoza-Macias");
+        //print out class name
+        System.out.println("Java Web");
+        //print out home town
+        System.out.println("Omaha, NE");
+        //print out my favorite dessert
+        System.out.println("Chocolate cake");
+    }
+}
---
+++ src/Assignment1_2.java
@@ -0,0 +1,13 @@
+public class Assignment1_2 {
+    
+    public static void main(String[] args) {
+        //print out first line
+        System.out.println("     J    A    V    V     A");
+        //print out second line
+        System.out.println("     J   A A   V    V    A A");
+        //print out third line
+        System.out.println("J    J  AAAAA   V   V   AAAAA");
+        //print out fourth line
+        System.out.println("  J J  A     A    V    A     A ");
+    }
+}
